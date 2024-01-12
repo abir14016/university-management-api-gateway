f4f4fa13450dca73fe6e66b86541bfd0c43de9f9
abir14016 <abir14016@gmail.com>
2024-01-13 01:18:55 +0600

    get single by id, update and delete academic semester

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -20,7 +20,37 @@ const getAllFromDB = async (req: Request, res: Response, next: NextFunction) => 
   }
 };
 
+const updateOneIntoDB = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AcademicSemesterService.updateOneIntoDB(req);
+    sendResponse(res, result);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getByIdFromDB = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AcademicSemesterService.getByIdFromDB(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const deleteByIdFromDB = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AcademicSemesterService.deleteByIdFromDB(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const AcademicSemesterController = {
   insertIntoDB,
-  getAllFromDB
+  getAllFromDB,
+  getByIdFromDB,
+  updateOneIntoDB,
+  deleteByIdFromDB
 };
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,9 +1,33 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import { AcademicSemesterValidation } from './academicSemester.validation';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import validateRequest from '../../middlewares/validateRequest';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
 router.get('/', AcademicSemesterController.getAllFromDB);
-router.post('/', AcademicSemesterController.insertIntoDB);
+router.get('/:id', AcademicSemesterController.getByIdFromDB);
+
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  validateRequest(AcademicSemesterValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.updateOneIntoDB
+);
+
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.deleteByIdFromDB
+);
 
 export const AcademicSemesterRoutes = router;
