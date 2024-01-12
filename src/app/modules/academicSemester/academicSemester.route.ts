de0d59234d1872036b4f04299070ddaedfe884b0
abir14016 <abir14016@gmail.com>
2024-01-13 01:08:28 +0600

    filter all academic semesters from core se service

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -3,6 +3,7 @@ import { AcademicSemesterController } from './academicSemester.controller';
 
 const router = express.Router();
 
+router.get('/', AcademicSemesterController.getAllFromDB);
 router.post('/', AcademicSemesterController.insertIntoDB);
 
 export const AcademicSemesterRoutes = router;
